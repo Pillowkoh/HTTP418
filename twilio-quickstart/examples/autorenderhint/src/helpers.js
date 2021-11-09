4b8da38342bb9bf3387c95d60358c429f8ea7d70
Fattyboy9898 <justin_peng@mymail.sutd.edu.sg>
2021-11-09 11:38:06 +0800

    first commit

---
+++ twilio-quickstart/examples/autorenderhint/src/helpers.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var Video = require('twilio-video');
+var VideoRoomMonitor = require('@twilio/video-room-monitor')
+
+/**
+ * Connect to a Room with 'auto' mode. This is the default mode.
+ * @param {string} token - AccessToken for joining the Room
+ * @returns {Room}
+ */
+function joinRoom(token) {
+  return Video.connect(token, {
+    name: 'my-cool-room',
+    bandwidthProfile: {
+      video: {
+        contentPreferencesMode: 'auto',
+        clientTrackSwitchOffControl: 'auto'
+      }
+    }
+  });
+}
+
+module.exports.joinRoom = joinRoom;
